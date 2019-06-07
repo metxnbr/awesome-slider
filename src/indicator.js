c9330baf0264e0414dd2ffad913312c10a6d7318
Jesse <le2op7y3vc859@foxmail.com>
2019-06-07 13:22:17 +0800

    fix indicator

--- src/indicator.js
+++ src/indicator.js
@@ -8,42 +8,42 @@ var options = {
     "display: inline-block;width: 7px;height: 7px;margin: 0 5px;border-radius: 50%;background-color: #fff;opacity: 0.8;"
 };
 
-function indicator() {
-  var ele = [];
-  return {
-    style: function() {
-      var wrap = document.createElement("div");
-      wrap.setAttribute("style", options.wrap);
-      this.eleCollections.listWrap.appendChild(wrap);
+function style() {
+  this.eleCollections.indicator = []
+  var wrap = document.createElement("div");
+  wrap.setAttribute("style", options.wrap);
+  this.eleCollections.listWrap.appendChild(wrap);
 
-      var list = document.createElement("div");
-      list.setAttribute("style", options.list);
-      wrap.appendChild(list);
+  var list = document.createElement("div");
+  list.setAttribute("style", options.list);
+  wrap.appendChild(list);
 
-      var i = 0;
-      while (i < this.realLen) {
-        var item = document.createElement("div");
-        if (i === 0) {
-          item.setAttribute("style", options.itemActive);
-        } else {
-          item.setAttribute("style", options.item);
-        }
-        list.appendChild(item);
-        ele.push(item)
-        i += 1;
-      }
-    },
-    active: function() {
-      var context = this;
-      ele.forEach(function(item, i) {
-        if (i === context.current - 1) {
-          item.setAttribute("style", options.itemActive);
-        } else {
-          item.setAttribute("style", options.item);
-        }
-      });
+  var i = 0;
+  while (i < this.realLen) {
+    var item = document.createElement("div");
+    if (i === 0) {
+      item.setAttribute("style", options.itemActive);
+    } else {
+      item.setAttribute("style", options.item);
     }
-  };
+    list.appendChild(item);
+    this.eleCollections.indicator.push(item)
+    i += 1;
+  }
 }
 
-module.exports = indicator();
+function active() {
+  var context = this;
+  this.eleCollections.indicator.forEach(function(item, i) {
+    if (i === context.current - 1) {
+      item.setAttribute("style", options.itemActive);
+    } else {
+      item.setAttribute("style", options.item);
+    }
+  });
+}
+
+module.exports = {
+  style: style,
+  active: active
+};
